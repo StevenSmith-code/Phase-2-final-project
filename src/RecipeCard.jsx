c400690579a2d6c7d0eda80c7f42ab22011a3923
stevensmith-code <digitalexile99@gmail.com>
2023-01-20 15:04:54 -0600

    finished styles for recipecard

--- src/RecipeCard.jsx
+++ src/RecipeCard.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 
-function RecipeCard() {
+function RecipeCard({id, img, title, ingredients, instructions, servings, cooktime}) {
   return (
-    <div className='bg-black h-96 w-80'>
-        
+    <div className='bg-gray-400 h-80 w-80 rounded-lg cursor-pointer hover:scale-110 transition duration-100 shadow-lg flex flex-col items-center'>
+        <img className='h-52 w-full p-2' src={img} alt="" />
+        <h1 className='text-sm sm:text-base xl:text-xl'>{title}</h1>
+        <div className='flex justify-around w-full mt-10'>
+            <p>Servings: {servings}</p>
+            <p>Cook Time: {cooktime}</p>
+        </div>
     </div>
   )
 }
--- src/RecipeContainer.jsx
+++ src/RecipeContainer.jsx
@@ -9,7 +9,7 @@ function RecipeContainer({data}) {
 
   return (
     <div className='bg-gray-200 h-[calc(100vh-80px)] flex items-center'>
-     <div className='w-11/12 h-[calc(100vh-80px)] mt-10 m-auto grid grid-cols-1 md:grid-cols-3 lg:grid-cols-4'>
+     <div className='w-11/12 h-[calc(100vh-80px)] mt-10 m-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4'>
       {renderCards}
      </div>
     </div>
